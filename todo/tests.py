4647fc5f1751258d7ecd617459d845261f40b0d5
1f10250073 <s1f102500731@iniad.org>
2026-06-25 11:00:45 +0900

    ソート機能を修正

--- todo/tests.py
+++ todo/tests.py
@@ -80,8 +80,8 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(response.context["tasks"][0], task2)
-        self.assertEqual(response.context["tasks"][1], task1)
+        self.assertEqual(response.context["tasks"][0], task1)
+        self.assertEqual(response.context["tasks"][1], task2)
 
     def test_index_get_order_due(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
